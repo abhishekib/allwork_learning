8964fa48d5e59103122eaa4c43ac34d3efe719d0
abhishekib <abhishekk@ibarts.in>
2024-11-16 00:05:29 +0530

    feat: created reusable custom appbar

---
+++ lib/widgets/custom_appbar.dart
@@ -0,0 +1,28 @@
+import 'package:allwork/utils/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MyWidget extends StatelessWidget {
+  final String title;
+  const MyWidget({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          Center(
+            child: Text(
+              title,
+              style: AppTextStyles.whiteBoldTitleText,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
