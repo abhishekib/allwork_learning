ad5252097f04beb92418a144760d618d3e147e25
abhishekib <abhishekk@ibarts.in>
2024-11-20 00:35:31 +0530

    UI: Fix Padding and Updated Ui viewing style for category list view

--- lib/utils/styles.dart
+++ lib/utils/styles.dart
@@ -1,9 +1,15 @@
+import 'package:allwork/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 // Define a reusable TextStyle for white bold text
 class AppTextStyles {
   static const TextStyle whiteBoldText = TextStyle(
     color: Colors.white,
+    fontWeight: FontWeight.bold,
+    fontSize: 20.0,
+  );
+  static const TextStyle blueBoldText = TextStyle(
+    color: AppColors.backgroundBlue,
     fontWeight: FontWeight.bold,
     fontSize: 20.0,
   );
--- lib/views/category_list_view.dart
+++ lib/views/category_list_view.dart
@@ -32,24 +32,54 @@ class CategoryListView extends StatelessWidget {
             style: AppTextStyles.whiteBoldTitleText,
           ), // Display the selected menu title here
         ),
-        body: ListView.builder(
-          itemCount: categoryItems.length,
-          itemBuilder: (context, index) {
-            final item = categoryItems[index];
-            return ListTile(
-              title: Text(
-                textCleanerController.cleanText(item.title),
-                style: AppTextStyles.whiteBoldText,
-              ),
-              onTap: () {
-                log("Category List View------>${item.title}");
-                Get.toNamed(
-                  '/category-detail',
-                  arguments: item,
-                );
-              },
-            );
-          },
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: ListView.builder(
+            itemCount: categoryItems.length,
+            itemBuilder: (context, index) {
+              final item = categoryItems[index];
+              return Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(9.5),
+                    ),
+                    child: ListTile(
+                      focusColor: Colors.purple,
+                      title: Text(
+                        textCleanerController.cleanText(item.title),
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                        style: AppTextStyles.blueBoldText,
+                      ),
+                      onTap: () {
+                        log("Category List View------>${item.title}");
+                        Get.toNamed(
+                          '/category-detail',
+                          arguments: item,
+                        );
+                      },
+                      trailing: Container(
+                        decoration: BoxDecoration(
+                          color: AppColors.backgroundBlue,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(3.0),
+                          child: Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10)
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
