15436a2ea6fa0a16d7333310977afb9f82baa54f
abhishekib <abhishekk@ibarts.in>
2024-11-29 04:58:56 +0530

    restruture: Updated Main Menu to disable scrolling and Updated Refresh method to be in Menu List View

--- lib/views/main_menu_view.dart
+++ lib/views/main_menu_view.dart
@@ -1,4 +1,3 @@
-import 'package:allwork/controllers/menu_list_controller.dart';
 import 'package:allwork/utils/colors.dart';
 import 'package:allwork/views/menu_list_view.dart';
 import 'package:allwork/widgets/background_wrapper.dart';
@@ -10,11 +9,8 @@ import 'package:allwork/widgets/search_bar_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:allwork/controllers/animated_text_controller.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import '../controllers/daily_date_controller.dart';
-import '../controllers/prayer_time_controller.dart';
 
 class MainMenuView extends StatefulWidget {
   const MainMenuView({super.key});
@@ -26,25 +22,6 @@ class MainMenuView extends StatefulWidget {
 class _MainMenuViewState extends State<MainMenuView> {
   String selectedLanguage = 'English';
   final animatedTextController = Get.put(AnimatedTextController());
-  final menuListController = Get.put(MenuListController());
-  final dailyDateController = Get.put(DailyDateController());
-  final prayerTimeController = Get.put(PrayerTimeController());
-
-  Future<void> _refreshData() async {
-    var connectivityResult = await Connectivity().checkConnectivity();
-    if (connectivityResult == ConnectivityResult.none) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-            content:
-                Text("No internet connection. Please check your network.")),
-      );
-    } else {
-      await animatedTextController.fetchTextData();
-      dailyDateController.fetchDailyDate();
-      prayerTimeController.fetchPrayerTimes(52.569500, -0.240530);
-      menuListController.fetchMenuItems();
-    }
-  }
 
   Future<void> _saveLanguagePreference(String language) async {
     final prefs = await SharedPreferences.getInstance();
@@ -57,126 +34,117 @@ class _MainMenuViewState extends State<MainMenuView> {
       child: Scaffold(
         drawer: CustomDrawer(),
         backgroundColor: AppColors.backgroundBlue,
-        body: RefreshIndicator(
-          onRefresh: _refreshData,
-          color: Colors.white,
-          child: CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: Container(
-                  color: AppColors.backgroundBlue,
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Row(
-                    children: [
-                      Builder(
-                        builder: (context) {
-                          return IconButton(
-                            icon: const Icon(Icons.menu, color: Colors.white),
-                            onPressed: () {
-                              Scaffold.of(context).openDrawer();
-                            },
+        body: SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Column(
+            children: [
+              // Static content that should not scroll
+              Container(
+                color: AppColors.backgroundBlue,
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  children: [
+                    Builder(
+                      builder: (context) {
+                        return IconButton(
+                          icon: const Icon(Icons.menu, color: Colors.white),
+                          onPressed: () {
+                            Scaffold.of(context).openDrawer();
+                          },
+                        );
+                      },
+                    ),
+                    Expanded(
+                      child: Obx(() {
+                        if (animatedTextController.isLoading.value) {
+                          return Container(
+                            height: 50.0,
+                            alignment: Alignment.center,
+                            child: const CircularProgressIndicator(),
                           );
-                        },
-                      ),
-                      Expanded(
-                        child: Obx(() {
-                          if (animatedTextController.isLoading.value) {
-                            return Container(
-                              height: 50.0,
-                              alignment: Alignment.center,
-                              child: const CircularProgressIndicator(),
-                            );
-                          } else if (animatedTextController
-                              .animatedTextList.isEmpty) {
-                            return const Center(
-                              child: Text(
-                                'No Data available',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            );
-                          } else {
-                            final marqueeTexts = animatedTextController
-                                .animatedTextList
-                                .map((text) => text)
-                                .toList();
-                            return MarqueeTextWidget(
-                              marqueeTexts: marqueeTexts,
-                            );
-                          }
-                        }),
-                      ),
-                    ],
-                  ),
+                        } else if (animatedTextController
+                            .animatedTextList.isEmpty) {
+                          return const Center(
+                            child: Text(
+                              'No Data available',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          );
+                        } else {
+                          final marqueeTexts = animatedTextController
+                              .animatedTextList
+                              .map((text) => text)
+                              .toList();
+                          return MarqueeTextWidget(marqueeTexts: marqueeTexts);
+                        }
+                      }),
+                    ),
+                  ],
                 ),
               ),
-              SliverToBoxAdapter(child: SearchBarWidget()),
-              SliverToBoxAdapter(child: DailyDateWidget()),
-              SliverToBoxAdapter(child: PrayerTimeWidget()),
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          const SizedBox(height: 10),
-                          Text(
-                            selectedLanguage == 'English'
-                                ? 'Language Selection : '
-                                : 'ભાષા પસંદગી : ',
+              SearchBarWidget(),
+              DailyDateWidget(),
+              PrayerTimeWidget(),
+
+              // Language selection dropdown
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        const SizedBox(height: 10),
+                        Text(
+                          selectedLanguage == 'English'
+                              ? 'Language Selection : '
+                              : 'ભાષા પસંદગી : ',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Text(
+                          'English & ગુજરાતી',
+                          style: TextStyle(fontSize: 18, color: Colors.white38),
+                        ),
+                      ],
+                    ),
+                    DropdownButton<String>(
+                      value: selectedLanguage,
+                      dropdownColor: AppColors.backgroundBlue,
+                      items:
+                          <String>['English', 'Gujarati'].map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
                             style: const TextStyle(
                               fontSize: 18,
                               color: Colors.white,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                          const Text(
-                            'English & ગુજરાતી',
-                            style:
-                                TextStyle(fontSize: 18, color: Colors.white38),
-                          ),
-                        ],
-                      ),
-                      DropdownButton<String>(
-                        value: selectedLanguage,
-                        dropdownColor: AppColors.backgroundBlue,
-                        items:
-                            <String>['English', 'Gujarati'].map((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(
-                              value,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          );
-                        }).toList(),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            selectedLanguage = newValue!;
-                            _saveLanguagePreference(selectedLanguage);
-                          });
-                        },
-                      ),
-                    ],
-                  ),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedLanguage = newValue!;
+                          _saveLanguagePreference(selectedLanguage);
+                        });
+                      },
+                    ),
+                  ],
                 ),
               ),
-              const SliverToBoxAdapter(child: SizedBox(height: 10)),
-              SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    return SizedBox(
-                      height: MediaQuery.of(context).size.height,
-                      child: MenuListView(selectedLanguage: selectedLanguage),
-                    );
-                  },
-                  childCount: 1,
-                ),
+
+              const SizedBox(height: 10),
+
+              // Menu List View
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: MenuListView(selectedLanguage: selectedLanguage),
               ),
             ],
           ),
--- lib/views/menu_list_view.dart
+++ lib/views/menu_list_view.dart
@@ -1,17 +1,30 @@
 import 'dart:developer';
+import 'package:allwork/controllers/animated_text_controller.dart';
 import 'package:allwork/controllers/category_list_controller.dart';
+import 'package:allwork/controllers/daily_date_controller.dart';
+import 'package:allwork/controllers/prayer_time_controller.dart';
 import 'package:allwork/utils/colors.dart';
 import 'package:allwork/utils/styles.dart';
 import 'package:allwork/views/category_list_view.dart';
 import 'package:allwork/views/menu_detail_view.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:allwork/controllers/menu_list_controller.dart';
 
-class MenuListView extends StatelessWidget {
+class MenuListView extends StatefulWidget {
   final String selectedLanguage;
 
   const MenuListView({super.key, required this.selectedLanguage});
+
+  @override
+  State<MenuListView> createState() => _MenuListViewState();
+}
+
+class _MenuListViewState extends State<MenuListView> {
+  final animatedTextController = Get.put(AnimatedTextController());
+  final dailyDateController = Get.put(DailyDateController());
+  final prayerTimeController = Get.put(PrayerTimeController());
 
   @override
   Widget build(BuildContext context) {
@@ -20,25 +33,48 @@ class MenuListView extends StatelessWidget {
         Get.put(CategoryListController()); // Use Get.put to persist controller
 
     Future<void> refreshMenuItems() async {
-      // Call the controller method to fetch the menu list again
-      controller.fetchMenuItems();
+      var connectivityResult = await Connectivity().checkConnectivity();
+      if (connectivityResult == ConnectivityResult.none) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content:
+                  Text("No internet connection. Please check your network.")),
+        );
+      } else {
+        await animatedTextController.fetchTextData();
+        dailyDateController.fetchDailyDate();
+        prayerTimeController.fetchPrayerTimes(52.569500, -0.240530);
+        controller.fetchMenuItems();
+      }
     }
 
     return Scaffold(
       backgroundColor: AppColors.backgroundBlue,
       body: Obx(() {
         // Use the selected language to choose the appropriate list
-        final menuList = selectedLanguage == 'English'
+        final menuList = widget.selectedLanguage == 'English'
             ? controller.menuList.value
             : controller.gujaratiMenuList.value;
 
         if (controller.isLoading.value) {
           return const Center(child: CircularProgressIndicator());
         } else if (menuList.items.isEmpty) {
-          return const Center(
-              child: Text(
-            "No menu items available",
-            style: AppTextStyles.whiteBoldText,
+          return Center(
+              child: Column(
+            children: [
+              const Text(
+                "No menu items available",
+                style: AppTextStyles.whiteBoldText,
+              ),
+              ElevatedButton(
+                onPressed: refreshMenuItems,
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+                child: const Text("Refresh"),
+              )
+            ],
           ));
         } else {
           return RefreshIndicator(
@@ -62,8 +98,7 @@ class MenuListView extends StatelessWidget {
 
                       Get.to(() => CategoryListView(
                             categoryItems:
-                                categoryListController.categoryData[""] ??
-                                    [],
+                                categoryListController.categoryData[""] ?? [],
                             argument: menuItem,
                           ));
                     } else {
