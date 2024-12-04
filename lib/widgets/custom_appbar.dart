f6b5c4777f980707254f68b6397a899192011191
allworkdone <raj635321@gmail.com>
2024-12-04 12:46:24 +0530

    wip: custom app bar: Modified Custom App Bar for proper navigation and text center

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -1,26 +1,44 @@
+import 'package:allwork/utils/colors.dart';
 import 'package:allwork/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class MyWidget extends StatelessWidget {
+class CustomAppbar extends StatelessWidget {
   final String title;
-  const MyWidget({super.key, required this.title});
+  const CustomAppbar({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          Icon(
-            Icons.arrow_back,
-            color: Colors.white,
-          ),
-          Center(
-            child: Text(
-              title,
-              style: AppTextStyles.whiteBoldTitleText,
+      backgroundColor: AppColors.backgroundBlue,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+        child: Stack(
+          children: [
+            // Back arrow aligned to the left
+            Align(
+              alignment: Alignment.centerLeft,
+              child: GestureDetector(
+                child: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                  size: 38,
+                ),
+                onTap: () {
+                  Get.back();
+                },
+              ),
             ),
-          ),
-        ],
+            // Title aligned to the center
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                title,
+                style: AppTextStyles.whiteBoldTitleText,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
