8bafc7434975468bb2da9a1de8d2c759449db5e3
abhishekib <abhishekk@ibarts.in>
2025-01-03 22:25:21 +0530

    feat: Add Gujarati labels for tabs when Gujarati language is selected
    - Dynamically map tab labels to Gujarati equivalents:
      - Arabic -> અરબી
      - Transliteration -> તરજુમા
      - Translation -> ગુજરાતી
    - Maintain default labels for other language selections.
    - Enhance user experience for Gujarati language users.

--- lib/views/category_detail_view.dart
+++ lib/views/category_detail_view.dart
@@ -311,7 +311,25 @@ class CategoryDetailViewState extends State<CategoryDetailView>
                 onTap: (index) {
                   controller.changeType(availableTypes[index]);
                 },
-                tabs: availableTypes.map((type) => Tab(text: type)).toList(),
+                // tabs: availableTypes.map((type) => Tab(text: type)).toList(),
+                tabs: availableTypes.map((type) {
+                  String tabLabel =
+                      type;
+                  if (selectedLanguage == 'Gujarati') {
+                    switch (type.toLowerCase()) {
+                      case 'arabic':
+                        tabLabel = 'અરબી';
+                        break;
+                      case 'transliteration':
+                        tabLabel = 'તરજુમા';
+                        break;
+                      case 'translation':
+                        tabLabel = 'ગુજરાતી';
+                        break;
+                    }
+                  }
+                  return Tab(text: tabLabel);
+                }).toList(),
               ),
               const SizedBox(height: 10),
               Flexible(
