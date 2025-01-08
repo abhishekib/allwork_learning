a9aaad47d6b384e2f82e4775645da15740d69ce2
abhishekib <abhishekk@ibarts.in>
2025-01-08 06:01:22 +0530

    feat: Implement dynamic sizing for PopupView to enhance responsiveness
    - Refactored PopupView to dynamically adjust its height and width based on the image dimensions and screen size using LayoutBuilder and ConstrainedBox.
    - Introduced _buildResponsiveImage method to handle image rendering with proper aspect ratio using Flexible and BoxFit.contain.
    - Added fallback for missing or failed image loading with appropriate error messages.
    - Removed hardcoded dimensions to ensure better cross-device compatibility.
    - Improved PopupView layout to dynamically adapt without disrupting the UI structure.

--- lib/views/event_popup_view.dart
+++ lib/views/event_popup_view.dart
@@ -7,7 +7,7 @@ import 'package:get/get.dart';
 class PopupView extends StatelessWidget {
   final PopupController eventPopupController = Get.put(PopupController());
 
-  PopupType popupType;
+  final PopupType popupType;
 
   PopupView(this.popupType, {super.key});
 
@@ -19,88 +19,123 @@ class PopupView extends StatelessWidget {
       },
       child: Dialog(
         backgroundColor: Colors.transparent,
-        child: Container(
-          width: 450,
-          decoration: BoxDecoration(
-            color: AppColors.backgroundBlue,
-            borderRadius: BorderRadius.circular(10.0),
-            // border: Border.all(color: Colors.white, width: 3),
-          ),
-          child: Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: constraints.maxWidth * 0.5, // 90% of the screen width
+                maxHeight:
+                    constraints.maxHeight * 0.48, // 90% of the screen height
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppColors.backgroundBlue,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Stack(
                   children: [
-                    Text(
-                      '', // You can dynamically change this text
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            '', // Dynamically change this text if needed
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          _buildPopupContent(context),
+                          const SizedBox(height: 10),
+                        ],
                       ),
                     ),
-                    SizedBox(height: 10),
-                    popupType == PopupType.AMAL_NAMAZ_POPUP
-                        ? Image.network(
-                            eventPopupController
-                                    .amalNamazPopupModel.value?.data ??
-                                '',
-                            fit: BoxFit.cover,
-                            height: 400,
-                          )
-                        : popupType == PopupType.EVENT_POPUP
-                            ? Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Image.network(
-                                    eventPopupController
-                                            .eventPopupModel.value?.imageUrl ??
-                                        '',
-                                    fit: BoxFit.cover,
-                                    height: 400,
-                                  ),
-                                ],
-                              )
-                            : Image.asset(
-                                'assets/images/start_popup_image.jpeg',
-                                fit: BoxFit.cover,
-                                height: 400,
-                              ),
-                    SizedBox(height: 10),
+                    Positioned(
+                      top: 10,
+                      right: 10,
+                      child: IconButton(
+                        icon: const Icon(Icons.close, color: Colors.white),
+                        onPressed: () {
+                          eventPopupController.closeBanner(); // Close the popup
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        child: Text(
+                          eventPopupController.eventPopupModel.value?.title ??
+                              '',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Text(
-                      eventPopupController.eventPopupModel.value?.title ?? '',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Positioned(
-                    right: 0,
-                    top: 0,
-                    child: IconButton(
-                      icon: Icon(Icons.close, color: Colors.white),
-                      onPressed: () {
-                        eventPopupController.closeBanner(); // Close the popup
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+            );
+          },
         ),
       ),
+    );
+  }
+
+  /// Builds the popup content dynamically based on the PopupType.
+  Widget _buildPopupContent(BuildContext context) {
+    if (popupType == PopupType.AMAL_NAMAZ_POPUP) {
+      final imageUrl = eventPopupController.amalNamazPopupModel.value?.data;
+      return _buildResponsiveImage(imageUrl);
+    } else if (popupType == PopupType.EVENT_POPUP) {
+      final imageUrl = eventPopupController.eventPopupModel.value?.imageUrl;
+      return _buildResponsiveImage(imageUrl);
+    } else {
+      return _buildResponsiveImage('assets/images/start_popup_image.jpeg',
+          isNetworkImage: false);
+    }
+  }
+
+  /// Dynamically builds a responsive image.
+  Widget _buildResponsiveImage(String? imageUrl, {bool isNetworkImage = true}) {
+    if (imageUrl == null || imageUrl.isEmpty) {
+      return const Text(
+        'No Image Available',
+        style: TextStyle(color: Colors.white),
+      );
+    }
+
+    return Flexible(
+      child: isNetworkImage
+          ? Image.network(
+              imageUrl,
+              fit: BoxFit.contain,
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress == null) return child;
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+              errorBuilder: (context, error, stackTrace) {
+                return const Center(
+                  child: Text(
+                    'Failed to load image',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                );
+              },
+            )
+          : Image.asset(
+              imageUrl,
+              fit: BoxFit.contain,
+            ),
     );
   }
 }
