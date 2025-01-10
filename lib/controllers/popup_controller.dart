c02395b2d4d1f6ef617bca5c9a0712a1d03cab78
abhishekib <abhishekk@ibarts.in>
2025-01-11 03:46:07 +0530

    fix: Updated Updated PopUp method for inital start and response based start  - removed shared preference implimentation and using direct method to always show image asset popup - removed controller closing method and using GetX navigation method to close banner - for static image given fix height and width - added WidgetsBinding.instance.addPostFrameCallback so the ui can build after fetching the data from all the api endpoint related to popup

--- lib/controllers/popup_controller.dart
+++ lib/controllers/popup_controller.dart
@@ -7,7 +7,6 @@ import 'package:allwork/utils/constants.dart';
 import 'package:allwork/utils/popupEnums.dart';
 import 'package:allwork/views/popup_view.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class PopupController extends GetxController {
   final isVisible = false.obs;
@@ -15,78 +14,42 @@ class PopupController extends GetxController {
   final Rx<AmalNamazPopupModel?> amalNamazPopupModel =
       Rx<AmalNamazPopupModel?>(null);
 
-  final PopupProvider _eventPopupProvider = PopupProvider(ApiConstants.token);
+  final PopupProvider _popupProvider = PopupProvider(ApiConstants.token);
 
   Future<void> fetchStartPopup() async {
-    // Obtain shared preferences instance
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    // Log the current state of 'startedPreviously'
-    bool startedPreviously = prefs.getBool('startedPreviously') ?? false;
-    log("Stated previously from shared preferences is $startedPreviously");
-
-    // Check if the app hasn't been started previously
-    if (!startedPreviously) {
-      // Mark 'startedPreviously' as true
-      await prefs.setBool('startedPreviously', true);
-
-      // Show the start popup
-      Get.dialog(PopupView(PopupType.START_POPUP), barrierDismissible: true);
-    }
+    await Get.dialog(
+      PopupView(PopupType.START_POPUP),
+      barrierDismissible: true,
+    );
   }
 
   Future<void> fetchAmalNamazPopup() async {
     try {
-      final response = await _eventPopupProvider.getAmalNamazPopup();
-      if (response != null) {
-        if(response.data.isNotEmpty) {
-          amalNamazPopupModel.value = response;
-          isVisible.value = true;
-          log("Event Popup Data: $response");
-
-          // Show the event popup dialog
-          Get.dialog(PopupView(PopupType.AMAL_NAMAZ_POPUP),
-              barrierDismissible: true);
-        }
-      } else {
-        // eventPopupModel.value = null;
-        isVisible.value = false;
-        log("No valid event data found.");
+      final response = await _popupProvider.getAmalNamazPopup();
+      if (response?.data?.isNotEmpty == true) {
+        amalNamazPopupModel.value = response;
+        await Get.dialog(
+          PopupView(PopupType.AMAL_NAMAZ_POPUP),
+          barrierDismissible: true,
+        );
       }
     } catch (e) {
-      //eventPopupModel.value = null;
-      isVisible.value = false;
-      log('Error fetching event popup: $e');
+      log('Error fetching Amal Namaz popup: $e');
     }
   }
 
   Future<void> fetchEventPopup() async {
     try {
-      final response = await _eventPopupProvider.getEventPopup();
-      if (response != null) {
-        if (response.imageUrl != null) {
-          eventPopupModel.value = response;
-          isVisible.value = true;
-          log("Event Popup Data: $response");
-
-          // Show the event popup dialog
-          Get.dialog(PopupView(PopupType.EVENT_POPUP),
-              barrierDismissible: true);
-        }
-      } else {
-        eventPopupModel.value = null;
-        isVisible.value = false;
-        log("No valid event data found.");
+      final response = await _popupProvider.getEventPopup();
+      if (response?.imageUrl?.isNotEmpty == true) {
+        eventPopupModel.value = response;
+        await Get.dialog(
+          PopupView(PopupType.EVENT_POPUP),
+          barrierDismissible: true,
+        );
       }
     } catch (e) {
-      //eventPopupModel.value = null;
-      isVisible.value = false;
-      log('Error fetching event popup: $e');
+      log('Error fetching Event popup: $e');
     }
-  }
-
-  void closeBanner() {
-    isVisible.value = false;
-    Get.back(); // Close the popup by popping the dialog
   }
 }
