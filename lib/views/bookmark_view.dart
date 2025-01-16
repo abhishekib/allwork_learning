fdf79441c0a76bc668e3b2d8f6152439ab6fb364
abhishekib <abhishekk@ibarts.in>
2025-01-17 04:13:12 +0530

    update: Updated UI for Booking Screen to match Existing Design Flow of Application

--- lib/views/bookmark_view.dart
+++ lib/views/bookmark_view.dart
@@ -1,4 +1,7 @@
 import 'package:allwork/controllers/bookmark_controller.dart';
+import 'package:allwork/controllers/category_list_controller.dart';
+import 'package:allwork/modals/category.dart';
+import 'package:allwork/utils/colors.dart';
 import 'package:allwork/utils/styles.dart';
 import 'package:allwork/widgets/background_wrapper.dart';
 import 'package:flutter/material.dart';
@@ -23,16 +26,78 @@ class BookmarkView extends StatelessWidget {
         body: Center(
           child: controller.bookmarks.isEmpty
               ? Text("No bookmarks saved", style: AppTextStyles.whiteBoldText)
-              : ListView.builder(
-                  itemCount: controller.bookmarks.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(onTap: () {
-                      controller.openBookmark(index);
+              : Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: ListView.builder(
+                    itemCount: controller.bookmarks.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(9.5),
+                            ),
+                            child: ListTile(
+                              onTap: () {
+                                controller.openBookmark(index);
+                              },
+                              title: Text(controller.bookmarks[index],
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: AppTextStyles.customStyle(
+                                    fontFamily: 'Roboto',
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: AppColors.backgroundBlue,
+                                  )),
+                              trailing: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                spacing: 10,
+                                children: [
+                                  Container(
+                                    padding: EdgeInsets.all(6),
+                                    decoration: BoxDecoration(
+                                      color: AppColors.backgroundBlue,
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: GestureDetector(
+                                      child: Icon(
+                                        Icons.bookmark_border,
+                                        color: Colors.white,
+                                        size: 16,
+                                      ),
+                                      onTap: () {
+                                        // final Category category =
+                                        //     Category.fromJson(item);
+                                        // CategoryListController
+                                        //     .saveCategoryListDetail(category);
+                                        // log(category.toString());
+                                      },
+                                    ),
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: AppColors.backgroundBlue,
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(3.0),
+                                      child: Icon(
+                                        Icons.arrow_forward_ios_rounded,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10)
+                        ],
+                      );
                     },
-                      title: Text(controller.bookmarks[index],
-                          style: AppTextStyles.whiteBoldText),
-                    );
-                  },
+                  ),
                 ),
         ),
       ),
