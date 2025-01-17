4dc74a6b2ef193c5b16e80d5fec7112f44984a9f
chittadeep intern <chittadeep@ibarts.in>
2025-01-17 12:56:14 +0530

    Hijri date getting saved in shared prefs

--- lib/controllers/hijri_date_adjustment_controller.dart
+++ lib/controllers/hijri_date_adjustment_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -16,17 +18,22 @@ class HijriDateAdjustmentController extends GetxController {
 
   RxString selectedAdjustment = RxString('0');
 
+  late SharedPreferences prefs;
+
   @override
   Future<void> onInit() async {
+    prefs = await SharedPreferences.getInstance();
+    selectedAdjustment.value = prefs.getString('hijri_date_adjustment') ?? '0';
     super.onInit();
   }
 
   Future<void> setSelectedAdjustment(String value) async {
     selectedAdjustment.value = value;
+    prefs.setString('hijri_date_adjustment', value);
+    log(value);
   }
 
   void closeDialogue() {
     Get.back();
   }
-
 }
