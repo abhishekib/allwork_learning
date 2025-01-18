4b95bfb6d3808d0136e56177a69bef1e9be5b1f8
allworkdone <raj635321@gmail.com>
2025-01-18 07:24:56 +0530

    Revert "configured the local notifications"
    This reverts commit f3043142fa072ccb0155e470a527b8427ab8bec0.

--- lib/main.dart
+++ lib/main.dart
@@ -5,40 +5,15 @@ import 'package:allwork/views/main_menu_view.dart';
 import 'package:allwork/views/menu_detail_view.dart';
 import 'package:allwork/views/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 import 'package:keep_screen_on/keep_screen_on.dart';
 import 'views/category_detail_view.dart';
 
-// Define the instance of the plugin
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-
-Future<void> main() async {
-  WidgetsFlutterBinding
-      .ensureInitialized(); // Ensure widgets are initialized before running the app
-
-  // Initialize notifications
-  await initNotifications();
-
+void main() {
   runApp(const MyApp());
   if (Platform.isAndroid || Platform.isIOS) {
     KeepScreenOn.turnOn();
   }
-}
-
-// Initialize the plugin with platform-specific settings
-Future<void> initNotifications() async {
-  const AndroidInitializationSettings initializationSettingsAndroid =
-      AndroidInitializationSettings(
-          'app_icon'); // 'app_icon' is your app's icon file
-
-  final InitializationSettings initializationSettings = InitializationSettings(
-    android: initializationSettingsAndroid,
-    //iOS: IOSInitializationSettings(),
-  );
-
-  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
 }
 
 class MyApp extends StatelessWidget {
