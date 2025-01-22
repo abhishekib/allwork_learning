54b48597aadaa46257c69ee2936f3ba015752e26
abhishekib <abhishekk@ibarts.in>
2025-01-23 03:00:32 +0530

    feat: Added Firebase in Project and Configured with Required configuration  - added google-service.json and its required configuration in build.gradle & settings.gradle  - created firebase_options.dart  - created firebase.json

--- android/app/src/main/kotlin/com/mafatihuljinan/org/MainActivity.kt
+++ android/app/src/main/kotlin/com/mafatihuljinan/org/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.allwork
+package com.mafatihuljinan.org
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import audioplayers_darwin
 import connectivity_plus
+import firebase_core
 import flutter_local_notifications
 import geolocator_apple
 import google_sign_in_ios
@@ -19,6 +20,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
